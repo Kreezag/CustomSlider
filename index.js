a397ca990845d9e0977b04cbd1bb38d41039910f
Andrey Kuchuk <kreezag@gmail.com>
2016-09-27 22:53:13 +0300

    create base style for cutom slider

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello you too');
+const slider = document.querySelectorAll('[data-slider]');
+const pointer = document.querySelectorAll('[data-pointer]');
+
+
+const arrowLeft = document.querySelectorAll('[data-arrow-left]');
+const arrowRight = document.querySelectorAll('[data-arrow-right]');
+
+console.log(arrowLeft);
+console.log(arrowRight);
+
+
+arrowLeft.onclick = function() {
+    console.log(slider)
+};
+
+arrowRight.onclick =function() {
+    console.log(pointer)
+};
