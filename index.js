4469d98095fa90ef2bf6996e3f1f97f4b1b45da4
Andrey Kuchuk <kreezag@gmail.com>
2016-10-06 12:27:00 +0300

    create change slide on crop click

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const ARROW_LEFT = document.querySelectorAll('[data-arrow-left]')[0];
 const ARROW_RIGHT = document.querySelectorAll('[data-arrow-right]')[0];
 
 const CROPS = document.querySelectorAll('[data-crop-list]')[0];
-
 const CROP_CLASS = 'b-slider__crop-item';
 
 
-function AddDataCrops (i) {
+
+
+function InitCrops (i) {
     const _elem = document.createElement('li');
 
     _elem.className = CROP_CLASS;
@@ -22,19 +23,24 @@ function AddDataCrops (i) {
     }
 }
 
-function AddDataSlides (i) {
+function InitSlides (i) {
     SLIDE[i].setAttribute('data-slide', i );
 }
 
-
-function SET_DATA_ATTR () {
+function Counter (e) {
     for (var _i = 0; _i < COUNT; _i++) {
-        AddDataCrops(_i);
-        AddDataSlides(_i);
+        e(_i);
     }
 }
 
+function SET_DATA_ATTR () {
+    Counter(InitCrops);
+    Counter(InitSlides);
+}
+
 SET_DATA_ATTR();
+
+
 
 
 
@@ -50,6 +56,14 @@ function setActive(el, position, boolean, activeClass) {
     if (!boolean && position == 0) {
         nextElem = el[(COUNT - 1)];
     }
+
+    nextElem.setAttribute(activeClass, '');
+}
+
+
+function setActiveWithCrop(el, position, activeClass) {
+
+    var nextElem = el[(+position)];
 
     nextElem.setAttribute(activeClass, '');
 }
@@ -71,15 +85,28 @@ function ChangeEl (boolean) {
 }
 
 
+
 CROPS.onclick = function(e) {
 
     if (e.target.getAttribute('data-crop')) { //HUCK
 
-        var _dataAttr = e.target.getAttribute('data-crop');
+        const _dataAttr = e.target.getAttribute('data-crop');
 
-        console.log(_dataAttr);
+        const _crop = CROPS.querySelectorAll('[data-crop]');
+        const activeSlide = SLIDER.querySelectorAll('[data-active]')[0];
+        const activeCrop = CROPS.querySelectorAll('[data-active]')[0];
+
+        if (_dataAttr !== activeSlide ) {
+            setActiveWithCrop(SLIDE, _dataAttr, 'data-active');
+            setActiveWithCrop(_crop, _dataAttr, 'data-active');
+
+            activeSlide.removeAttribute('data-active');
+            activeCrop.removeAttribute('data-active');
+        }
     }
 };
+
+
 
 
 ARROW_LEFT.onclick = () => ChangeEl(false);
